7a23944fe656577cb590c7b519c0d7051ce295a7
LANG <honflang@outlook.com>
2025-11-11 10:13:02 +0800

    feat: add support for multiple sheets and data extraction

--- README.md
+++ README.md
@@ -22,6 +22,8 @@ pip install pandas openpyxl
 
 ## Config
 
+### Main config
+
 - `main_compare_file_path`
 
   主文件路径（生成的对比结果文件会以此文件为基础）
@@ -30,25 +32,11 @@ pip install pandas openpyxl
 
   副文件路径
 
-- `unique_columns`
-
-  作为联合唯一业务标识的列
-
-- `compare_columns`
-
-  数据对比的列
-
 - `output_path`
 
   **默认值**：""
 
   对比结果文件的生成路径（无需指定文件名）(未配置时会在当前命令执行目录生成)
-
-- `skip_lines`
-
-  **默认值**：0
-
-  对比时跳过的行数（第一行默认为表头默认跳过），例如需要跳过第二行时，配置此参数为 1
 
 - `skip_both`
 
@@ -56,23 +44,159 @@ pip install pandas openpyxl
 
   在跳过时，副文件是否同时跳过
 
+### Sheets config
+
+- `index`
+
+  sheet 的 index，从 0 开始
+
+- `name`
+
+  sheet 的名称，当和 `index` 同时存在时，以 `index` 为准
+
+- `skip_lines`
+
+  **默认值**：0
+
+  对比时跳过的行数（第一行默认为表头默认跳过），例如需要跳过第二行时，配置此参数为 1
+
+- `unique_columns`
+
+  作为联合唯一业务标识的列
+
+  > [!NOTE]
+  >
+  > 1、定义栏位的结构可以为 `Colums config` 或 `字符串` 
+  >
+  > 2、为 `字符串` 时直接使用对应单元格的原始值进行处理
+  >
+  > 3、`Colums config` 可以和普通字符串同时出现在数组中
+
+- `compare_columns`
+
+  数据对比的列
+
+  > [!NOTE]
+  >
+  > 说明同 `unique_columns`
+
+### Colums config
+
+- `name`
+
+  列名
+
+- `sub`
+
+  非必需，截取规则为：
+
+  - 当数组长度为 1 时，截取前 n 位，例如截取前 2 位则设置为  [ 2 ]
+  - 当数组长度为 2 时，按照指定指定区间进行截取，例如截取 2 至 5 位则设置为 [ 1, 5 ]
+  - 当数组长度为 2 且第一个元素为 null 时，可截取后 n 位，例如截取后 2 位则设置为 [ null, -2 ]
+
 ```json
 {
-  "main_compare_file_path": "your path/main.xlsx",
-  "sub_compare_file_path": "your path/sub.xlsx",
-  "output_path": "your path/",
-  "skip_lines": 3,
-  "skip_both": true,
-  "unique_columns": [
-    "部门",
-    "系统名称",
-    "系统名称2"
-  ],
-  "compare_columns": [
-    "阶段1",
-    "阶段2",
-    "阶段3"
-  ]
+    "main_compare_file_path": "your path/e1.xlsx",
+    "sub_compare_file_path": "your path/e2.xlsx",
+    "output_path": "your path/",
+    "skip_both": true,
+    "sheets": [
+        {
+            "index": 1,
+            "name": "Sheet1",
+            "skip_lines": 0,
+            "unique_columns": [
+                {
+                    "name": "部门",
+                    "sub": [
+                        null,
+                        -2
+                    ]
+                },
+                "系统"
+            ],
+            "compare_columns": [
+                {
+                    "name": "状态1",
+                    "sub": [
+                        2
+                    ]
+                },
+                {
+                    "name": "状态2",
+                    "sub": [
+                        1
+                    ]
+                }
+            ]
+        },
+        {
+            "index": 0,
+            "skip_lines": 3,
+            "unique_columns": [
+                "部门",
+                "系统名称",
+                "系统名称2"
+            ],
+            "compare_columns": [
+                "阶段1",
+                "阶段2",
+                "阶段3"
+            ]
+        },
+        {
+            "name": "Sheet1",
+            "unique_columns": [
+                "部门",
+                "系统名称",
+                "系统名称2"
+            ],
+            "compare_columns": [
+                "阶段1",
+                "阶段2",
+                "阶段3"
+            ]
+        },
+        {
+            "unique_columns": [
+                "部门",
+                "系统名称",
+                "系统名称2"
+            ],
+            "compare_columns": [
+                "阶段1",
+                "阶段2",
+                "阶段3"
+            ]
+        },
+        {
+            "index": -1,
+            "unique_columns": [
+                "部门",
+                "系统名称",
+                "系统名称2"
+            ],
+            "compare_columns": [
+                "阶段1",
+                "阶段2",
+                "阶段3"
+            ]
+        },
+        {
+            "index": -1,
+            "name": "S1",
+            "unique_columns": [
+                "部门",
+                "系统名称",
+                "系统名称2"
+            ],
+            "compare_columns": [
+                "阶段1",
+                "阶段2",
+                "阶段3"
+            ]
+        }
+    ]
 }
 ```
 
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import json
 import os
 import shutil
 import sys
+import traceback
 from copy import copy
 from dataclasses import dataclass
 from datetime import datetime
@@ -9,57 +10,125 @@ from datetime import datetime
 import openpyxl
 import pandas as pd
 from openpyxl.comments import Comment
-from openpyxl.styles import Color, Font, PatternFill
+from openpyxl.styles import Font, PatternFill
 
 def_config_file_name = 'config.json'
 config = None
+compared_sheet_name = []
+
+@dataclass
+class ColumnConfig:
+    name: str = None
+    sub: list[int] = None
+
+    @classmethod
+    def from_json(cls, json_data):
+        if isinstance(json_data, str):
+            return cls(name=json_data)
+        else:
+            return cls(name=json_data['name'],
+                       sub=json_data.get('sub'))
+
+
+@dataclass
+class SheetConfig:
+    unique_columns: list[ColumnConfig]
+    compare_columns: list[ColumnConfig]
+    index: int = None
+    name: str = None
+    skip_lines: int = 0  # 跳过的数据行数（除表头外也就是从第二行开始计算）
+
+    @classmethod
+    def from_json(cls, json_data):
+        unique_columns = [ColumnConfig.from_json(unique_column) for unique_column in json_data['unique_columns']]
+        compare_columns = [ColumnConfig.from_json(compare_column) for compare_column in json_data['compare_columns']]
+        return cls(unique_columns=unique_columns,
+                   compare_columns=compare_columns,
+                   index=json_data.get('index'),
+                   name=json_data.get('name'),
+                   skip_lines=json_data.get('skip_lines')
+                   )
 
 
 @dataclass
 class Config:
     main_compare_file_path: str  # 主文件名
     sub_compare_file_path: str  # 副文件名
-    unique_columns: list  # 联合唯一栏位
-    compare_columns: list  # 需要对比的栏位
+    sheets: list[SheetConfig]  # Sheet 配置
     output_path: str = ""  # 对比文件输出位置
-    skip_lines: int = 0  # 跳过的数据行数（除表头外也就是从第二行开始计算）
     skip_both: bool = True
     result_file_ptah: str = None  # 对比结果文件名
 
     @classmethod
     def from_json(cls, json_data):
-        return cls(**json_data)
+        sheets = [SheetConfig.from_json(sheet) for sheet in json_data['sheets']]
+        return cls(
+            main_compare_file_path=json_data['main_compare_file_path'],
+            sub_compare_file_path=json_data['sub_compare_file_path'],
+            output_path=json_data['output_path'],
+            skip_both=json_data['skip_both'],
+            sheets=sheets
+        )
 
 
 def compare():
-    main_sheet = read_excel(config.main_compare_file_path, 0)
-    sub_sheet = read_excel(config.sub_compare_file_path, 0)
+    copy_file_shutil_copy()
+    for sheet_config in config.sheets:
+        compare_sheet(sheet_config)
 
-    if main_sheet is None or sub_sheet is None:
+
+def compare_sheet(sheet_config: SheetConfig):
+    # TODO 是否可以避免重复加载保存结果文件
+    sheet_index = sheet_config.index if (
+            sheet_config.index is not None and sheet_config.index >= 0) else sheet_config.name
+    if sheet_index is None:
+        print(f"[Warn] Invalid sheet index : {sheet_index}")
+        return None
+
+    main_sheet = read_excel(config.main_compare_file_path, sheet_index)
+    sub_sheet = read_excel(config.sub_compare_file_path, sheet_index)
+
+    if main_sheet is None:
+        print(f"[Warn] Sheet '{sheet_index}' not found in '{config.main_compare_file_path}'")
+        return None
+
+    if sub_sheet is None:
+        print(f"[Warn] Sheet '{sheet_index}' not found in '{config.sub_compare_file_path}'")
         return None
 
     try:
-        missing_columns = set(config.unique_columns) - set(main_sheet.columns)
+        unique_columns_name = [col.name for col in sheet_config.unique_columns]
+        missing_columns = set(unique_columns_name) - set(main_sheet.columns)
         if missing_columns:
-            print(f"文件 '{config.main_compare_file_path}' 缺少必要的列: {', '.join(missing_columns)}")
-        missing_columns_2 = set(config.unique_columns) - set(sub_sheet.columns)
+            print(
+                f"[Warn] File '{config.main_compare_file_path}' sheet[{sheet_index}] 缺少必要的列: {', '.join(missing_columns)}")
+        missing_columns_2 = set(unique_columns_name) - set(sub_sheet.columns)
         if missing_columns_2:
-            print(f"文件 '{config.sub_compare_file_path}' 缺少必要的列: {', '.join(missing_columns_2)}")
+            print(
+                f"[Warn] File '{config.sub_compare_file_path}' sheet[{sheet_index}] 缺少必要的列: {', '.join(missing_columns_2)}")
 
         if missing_columns or missing_columns_2:
             return None
 
-        main_sheet_data = get_sheet_data(main_sheet)
-        sub_sheet_data = get_sheet_data(sub_sheet)
+        main_sheet_data = get_sheet_data(main_sheet, sheet_config)
+        sub_sheet_data = get_sheet_data(sub_sheet, sheet_config)
 
-        # print(f"main_sheet_data : {main_sheet_data}")
-        # print(f"sub_sheet_data : {sub_sheet_data}")
-
-        copy_file_shutil_copy()
         workbook = openpyxl.load_workbook(config.result_file_ptah)
-        result_sheet = workbook.worksheets[0]
+
+        if isinstance(sheet_index, str):
+            result_sheet = workbook[sheet_index]
+        else:
+            result_sheet = workbook.worksheets[sheet_index]
+
+        result_sheet_name = result_sheet.title
+        if result_sheet_name in compared_sheet_name:
+            print(f"[Warn] Sheet '{result_sheet_name}' already compared")
+            return None
+
+        compared_sheet_name.append(result_sheet_name)
+
         result_sheet.insert_cols(1, 2)
-        result_sheet['A1'].value = list_to_str(config.unique_columns)
+        result_sheet['A1'].value = list_to_str(unique_columns_name)
         result_sheet['B1'].value = "对比结果"
         result_sheet.column_dimensions['A'].width = 40
         result_sheet.column_dimensions['B'].width = 20
@@ -70,13 +139,20 @@ def compare():
         match_to_multiple_lines = []
         df_count = 0
         eq_count = 0
+        repetition_count = 0
         m_overflowing_count = 0
         m_max_len = len(main_sheet_data["unique_values"])
         for i in range(0, m_max_len):
-            if i < config.skip_lines:
+            if i < sheet_config.skip_lines:
                 continue
             mu = main_sheet_data["unique_values"][i]
             result_sheet.cell(row=i + 2, column=1, value=list_to_str(mu))
+
+            if main_sheet_data["unique_values"].count(mu) > 1:
+                result_sheet.cell(row=i + 2, column=2, value="当前表重复")
+                repetition_count += 1
+                continue
+
             try:
                 index = sub_sheet_data["unique_values"].index(mu)
                 if sub_sheet_data["unique_values"].count(mu) > 1:
@@ -94,7 +170,7 @@ def compare():
                                 cell = result_sheet.cell(row=i + 2, column=main_sheet_data["compare_indices"][j] + 3)
                                 cell.fill = PatternFill(start_color="FFFF00", end_color="FFFF00", fill_type="solid")
                                 cell.comment = Comment(
-                                    f"当前值：{main_sheet_data["compare_values"][i][j]}\r他表值：{sub_sheet_data["compare_values"][i][j]}",
+                                    f"当前值：{main_sheet_data["compare_values"][i][j]}\r\n他表值：{sub_sheet_data["compare_values"][i][j]}",
                                     "<System>")
             except ValueError:
                 result_sheet.cell(row=i + 2, column=2, value="他表缺失")
@@ -103,7 +179,7 @@ def compare():
         s_overflowing_count = 0
         s_max_len = len(sub_sheet_data["unique_values"])
         for i in range(0, s_max_len):
-            if config.skip_both and i < config.skip_lines:
+            if config.skip_both and i < sheet_config.skip_lines:
                 continue
             su = sub_sheet_data["unique_values"][i]
             try:
@@ -117,25 +193,28 @@ def compare():
         # print(f"m_overflowing_lines : {m_overflowing_lines}")
 
         total_line_index = m_max_len + s_overflowing_count
-        result_sheet['A' + str(total_line_index + 3)].value = "总行数：" + str(total_line_index - config.skip_lines)
+        result_sheet['A' + str(total_line_index + 3)].value = "总行数：" + str(
+            total_line_index - sheet_config.skip_lines)
         result_sheet['A' + str(total_line_index + 4)].value = "一致数：" + str(eq_count)
         result_sheet['A' + str(total_line_index + 5)].value = "不一致数：" + str(df_count)
         result_sheet['A' + str(total_line_index + 6)].value = "他表缺失数：" + str(m_overflowing_count)
         result_sheet['A' + str(total_line_index + 7)].value = "当前表缺失数：" + str(s_overflowing_count)
-        result_sheet['A' + str(total_line_index + 8)].value = "他表匹配到多列数：" + str(len(match_to_multiple_lines))
+        result_sheet['A' + str(total_line_index + 8)].value = "当前表重复行数：" + str(repetition_count)
+        result_sheet['A' + str(total_line_index + 9)].value = "他表匹配到多列数：" + str(len(match_to_multiple_lines))
 
-        result_sheet['A' + str(total_line_index + 3)].font = Font(color="FF0000")
-        result_sheet['A' + str(total_line_index + 4)].font = Font(color="FF0000")
+        # result_sheet['A' + str(total_line_index + 3)].font = Font(color="FF0000")
+        result_sheet['A' + str(total_line_index + 4)].font = Font(color="00FF00")
         result_sheet['A' + str(total_line_index + 5)].font = Font(color="FF0000")
         result_sheet['A' + str(total_line_index + 6)].font = Font(color="FF0000")
         result_sheet['A' + str(total_line_index + 7)].font = Font(color="FF0000")
         result_sheet['A' + str(total_line_index + 8)].font = Font(color="FF0000")
+        result_sheet['A' + str(total_line_index + 9)].font = Font(color="FF0000")
 
         workbook.save(config.result_file_ptah)
 
-        print(f"Successfully compare")
+        print(f"[Info] Successfully compared sheet '{result_sheet_name}'")
     except KeyError as e:
-        print(f"Error: A required column was not found. Please check your column names. Details: {e}")
+        print(f"A required column was not found. Please check your column names. Details: {e}")
     except Exception as e:
         print(f"An unexpected error occurred: {e}")
 
@@ -147,26 +226,44 @@ def list_to_str(l):
         return '/'.join([str(x) if x is not None else '空' for x in l])
 
 
-def get_sheet_data(sheet):
-    if sheet is None:
+def get_sheet_data(sheet, sheet_config: SheetConfig):
+    if sheet is None or sheet_config is None:
         return None
     sheet_data = {"unique_values": [], "compare_values": []}
     compare_indices = []
-    column_data = sheet[config.unique_columns[0]]
+    column_data = sheet[sheet_config.unique_columns[0].name]
     for i in range(len(column_data)):
         unique_value = []
         compare_value = []
-        for j in range(len(config.unique_columns)):
-            unique_value.append(nan_as_none(sheet[config.unique_columns[j]][i]))
-        for k in range(len(config.compare_columns)):
+        for j in range(len(sheet_config.unique_columns)):
+            # unique_value.append(nan_as_none(sheet[sheet_config.unique_columns[j]][i]))
+            unique_value.append(get_cell_value(sheet, i, sheet_config.unique_columns[j]))
+        for k in range(len(sheet_config.compare_columns)):
             if i == 0:
-                compare_indices.append(sheet.columns.get_loc(config.compare_columns[k]))
-            compare_value.append(nan_as_none(sheet[config.compare_columns[k]][i]))
+                compare_indices.append(sheet.columns.get_loc(sheet_config.compare_columns[k].name))
+            # compare_value.append(nan_as_none(sheet[sheet_config.compare_columns[k]][i]))
+            compare_value.append(get_cell_value(sheet, i, sheet_config.compare_columns[k]))
         sheet_data["unique_values"].append(unique_value)
         sheet_data["compare_values"].append(compare_value)
 
     sheet_data["compare_indices"] = compare_indices
     return sheet_data
+
+
+def get_cell_value(sheet, line: int, column_config: ColumnConfig):
+    val = nan_as_none(sheet[column_config.name][line])
+    if val is None:
+        return None
+    else:
+        if column_config.sub is not None and len(column_config.sub) > 0:
+            if len(column_config.sub) == 1:
+                return str(val)[:column_config.sub[0]]
+            if len(column_config.sub) == 2:
+                if column_config.sub[0] is None:
+                    return str(val)[column_config.sub[1]:]
+                else:
+                    return str(val)[column_config.sub[0]:column_config.sub[1]]
+        return val
 
 
 def nan_as_none(value):
@@ -188,11 +285,8 @@ def complete_file_path(path=""):
 def read_excel(filename, index):
     try:
         return pd.read_excel(filename, sheet_name=index)
-    except FileNotFoundError:
-        print(f"Error: The file '{filename}' was not found.")
-        return None
     except Exception as e:
-        print(f"An unexpected error occurred: {e}")
+        # print(f"  --  An unexpected error occurred: {e}")
         return None
 
 
@@ -227,7 +321,7 @@ def load_config(file_path=None):
                 "-%Y%m%d%H%M%S.")
                                    + main_compare_file_name.split(
                 '.')[1])
-    print(f"Loaded config : {new_config}")
+    # print(f"Loaded config : {new_config}")
     return new_config
 
 
@@ -235,11 +329,11 @@ def copy_file_shutil_copy():
     try:
         shutil.copy(config.main_compare_file_path, config.result_file_ptah)
     except FileNotFoundError:
-        print(f"错误: 源文件 '{config.main_compare_file_path}' 未找到。")
+        print(f"  --  Error: 源文件 '{config.main_compare_file_path}' 未找到。")
     except IsADirectoryError:
-        print(f"错误: 目标 '{config.result_file_ptah}' 是一个目录，但你指定了文件作为目标。")
+        print(f"  --  Error: 目标 '{config.result_file_ptah}' 是一个目录，但你指定了文件作为目标。")
     except Exception as e:
-        print(f"复制文件时发生错误: {e}")
+        print(f"  --  复制文件时发生错误: {e}")
 
 
 def copy_cell_style(source_cell, target_cell):
@@ -274,8 +368,9 @@ if __name__ == "__main__":
         else:
             config = load_config()
     except Exception as ex:
-        print(f"Load config file failed")
+        print(f"  --  Load config file failed")
         print(ex)
+        traceback.print_exc()
         sys.exit(1)
     try:
         compare()
